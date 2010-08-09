591bfcb1cb048bbf666c78035a0dfa2bc5fcfb3c
FND <fnd@fnd-oda.(none)>
2010-08-09 10:02:26 +0200

    Thunderbird: customized reply header

---
+++ .thunderbird/fnd/user.js
@@ -0,0 +1,24 @@
+// customized reply header
+user_pref("mailnews.reply_header_type", 3);
+user_pref("mailnews.reply_header_authorwrote", "----- Original Message -----\nFrom: %s");
+user_pref("mailnews.reply_header_ondate", "Date: %s");
+user_pref("mailnews.reply_header_separator", "\n");
+user_pref("mailnews.reply_header_colon", "");
+
+// display time of new mail notification
+//user_pref("alerts.totalOpenTime", 4000);
+
+// deactivate quote bar
+//ser_pref("mailnews.display.disable_format_flowed_support", true);
+//ser_pref("mail.quoted_graphical", false);
+
+// sorting threads
+//ser_pref("mailnews.thread_pane_column_unthreads", false);
+
+// purge download manager
+user_pref("browser.download.manager.retention", 0);
+user_pref("browser.download.manager.closeWhenDone", true);
+
+// address book settings
+//user_pref("mail.addr_book.displayName.lastnamefirst", true);
+//user_pref("mail.addr_book.lastnamefirst", 1);
