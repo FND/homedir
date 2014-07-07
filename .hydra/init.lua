63d1b8f99148b05d38eec824160cbd78ca6b885b
FND <fnd@localhost.localdomain>
2014-07-07 11:04:41 +0200

    extended Hydra config
    not DRY, but whatever

--- .hydra/init.lua
+++ .hydra/init.lua
@@ -19,11 +19,56 @@ menu.show(function()
 	}
 end)
 
+-- move to lower right
 hotkey.new({ "cmd", "ctrl", "alt" }, "right", function()
 	local win = window.focusedwindow()
 	local frame = win:frame()
 	local screen = win:screen():frame()
 	frame.x = screen.w - frame.w
 	frame.y = screen.h - frame.h
+	win:setframe(frame)
+end):enable()
+
+-- maximize
+hotkey.new({ "cmd", "ctrl", "alt" }, "up", function()
+	local win = window.focusedwindow()
+	local frame = win:frame()
+	local screen = win:screen():frame()
+	frame.x = 0
+	frame.y = 0
+	frame.w = screen.w
+	frame.h = screen.h
+	win:setframe(frame)
+end):enable()
+
+-- move up
+hotkey.new({ "cmd", "ctrl", "shift" }, "up", function()
+	local win = window.focusedwindow()
+	local frame = win:frame()
+	frame.y = frame.y - 1
+	win:setframe(frame)
+end):enable()
+
+-- move down
+hotkey.new({ "cmd", "ctrl", "shift" }, "down", function()
+	local win = window.focusedwindow()
+	local frame = win:frame()
+	frame.y = frame.y + 1
+	win:setframe(frame)
+end):enable()
+
+-- move left
+hotkey.new({ "cmd", "ctrl", "shift" }, "left", function()
+	local win = window.focusedwindow()
+	local frame = win:frame()
+	frame.x = frame.x - 1
+	win:setframe(frame)
+end):enable()
+
+-- move right
+hotkey.new({ "cmd", "ctrl", "shift" }, "right", function()
+	local win = window.focusedwindow()
+	local frame = win:frame()
+	frame.x = frame.x + 1
 	win:setframe(frame)
 end):enable()
