f1fe0f0da08eb4f24fbfb91fa702a5a1dfb74bf1
FND <fnd@localhost.localdomain>
2016-02-24 09:46:05 +0100

    added vertical splits to Mjolnir config

--- .mjolnir/init.lua
+++ .mjolnir/init.lua
@@ -20,11 +20,33 @@ hotkey.bind({"cmd", "alt", "ctrl"}, "up", function()
 end)
 
 -- move to lower right
-hotkey.bind({"cmd", "alt", "ctrl"}, "right", function()
-	local win = window.focusedwindow()
-	local frame = win:frame()
-	local screen = win:screen():fullframe()
+hotkey.bind({"cmd", "alt", "ctrl"}, "down", function()
+	local win, frame, screen = getDimensions()
 	frame.x = screen.w - frame.w
 	frame.y = screen.h - frame.h
 	win:setframe(frame)
 end)
+
+-- vertical split
+hotkey.bind({"cmd", "alt", "ctrl"}, "right", function()
+	local win, frame, screen = getDimensions()
+	local half_screen = screen.w / 2
+
+	-- toggle position (left / right side)
+	if frame.x == half_screen and frame.w == half_screen then
+		frame.x = 0
+	else
+		frame.x = half_screen
+	end
+
+	frame.y = 0
+	frame.w = half_screen
+	win:setframe(frame)
+end)
+
+function getDimensions()
+	local win = window.focusedwindow()
+	local frame = win:frame()
+	local screen = win:screen():fullframe()
+	return win, frame, screen
+end
